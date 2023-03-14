fa1d84764b2cb2664be7436f103a08ac89f6ac2b
Max Clinton <maxtclinton@gmail.com>
2023-03-13 22:02:33 -0400

    Adding search for pokemon

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+"""Termdex displays information about pokemon"""
+import requests
+
+
+def search_pokemon(pokemon_id):
+    """Search for information regarding pokemon"""
+    res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+    body = res.json()
+    print(body["name"])
+
 
 def main():
+    """Run Termdex app"""
     print("Welcome to TermDex")
     print("Input a pokemon ID")
+    pokemon_id = input()
+    pokemon_id = int(pokemon_id)
+    search_pokemon(pokemon_id)
 
 
 if __name__ == "__main__":
