4a9c6f21ec6bcf6b414bfdcb076c0df2fdfbd4f0
Max Clinton <maxtclinton@gmail.com>
2023-03-13 22:21:12 -0400

    Display pokemon sprite

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 """Termdex displays information about pokemon"""
+import json
+
 import requests
+
+
+def show_pokemon(pokemon_id):
+    """Read pokemon sprites from json file"""
+    with open("pokemon.json", encoding="utf-8") as pokemon_data:
+        data = json.load(pokemon_data)
+    print(data[str(pokemon_id)])
 
 
 def search_pokemon(pokemon_id):
     """Search for information regarding pokemon"""
     res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
     body = res.json()
+    pokemon_types = []
+    for ptype in body["types"]:
+        pokemon_types.append(ptype["type"]["name"])
+    show_pokemon(pokemon_id)
     print(body["name"])
 
 
